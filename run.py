a0171db0f0cfe8f29befc737ba969e7eff9a10ed
previoip <previo.prakasa@gmail.com>
2025-12-11 21:44:25 +0700

    refurbish

--- run.py
+++ run.py
@@ -7,8 +7,7 @@ def module_from_file(module_name, file_path):
   spec.loader.exec_module(module)
   return module
 
-
-def build_argparser():
+def init_argparser():
   from argparse import ArgumentParser
   parser = ArgumentParser(prog='aoc')
   parser.add_argument('modulepath')
@@ -19,16 +18,29 @@ def build_argparser():
   parser.add_argument('-a', '--all', action='store_true', help='multiline input')
   return parser
 
+def validate_modulepath(path):
+  pwd = os.path.dirname(os.path.abspath(__file__))
+  path = os.path.abspath(path)
+  return os.path.commonpath((pwd, path)) == pwd
+
+
 if __name__ == '__main__':
-  parser = build_argparser()
+  parser = init_argparser()
   args = parser.parse_args()
 
-  if not os.path.exists(args.modulepath):
-    raise FileNotFoundError(f'cannot relocate module {args.modulepath}')
+  if not validate_modulepath(args.modulepath):
+    raise RuntimeError('import from outside this directory is not allowed')
 
-  module = module_from_file('_module', args.modulepath)
+  if not os.path.exists(args.modulepath):
+    raise FileNotFoundError(f'cannot locate module {args.modulepath}')
+
+  module = module_from_file('puzzle', args.modulepath)
   if not hasattr(module, args.function):
-    raise AttributeError(f'module has no attribute {args.function}')
+    attrs = filter(lambda s: not s.startswith('_') and callable(getattr(module, s)), dir(module))
+    raise AttributeError(f'module has no attribute "{args.function}"\naccessible callable attributes:\n\t{"\n\t".join(attrs)}')
+
+  if args.function.startswith('_'):
+    raise AttributeError('target module attribute is a private member')
 
   function = getattr(module, args.function)
   def runfunc(inp):
@@ -45,7 +57,7 @@ if __name__ == '__main__':
 
   if not args.file is None:
     if not os.path.exists(args.file) or not os.path.isfile(args.file):
-      raise FileNotFoundError(f'cannot relocate file {args.file}')
+      raise FileNotFoundError(f'cannot locate file {args.file}')
     with open(args.file, 'r') as fp:
       if args.all:
         runfunc(fp.read())
--- shared/sqlite3w.py
+++ shared/sqlite3w.py
@@ -1,28 +1,72 @@
 import sqlite3
+import os
+from threading import Lock
 
+def sqlite3w_classmethod_closedconn_fallback_value(value):
+  def decorator(func):
+    def methodwrapper(self, *args, **kwargs):
+      if self._isclosed:
+        return value
+      try:
+        retv = func(self, *args, **kwargs)
+      except sqlite3.ProgrammingError:
+        print('warning: connection closed but flag is unset')
+        self._isclosed = True
+        retv = value
+      return retv
+    return methodwrapper
+  return decorator
 
-class SqlLite3Queue:
+class _SqlLite3TempConn:
   _instances = list()
+  _thread_lock = Lock()
 
-  def __init__(self, path):
-    self._instances.append(self)
+  @classmethod
+  def _handle_closeAllOpenConnInstance(cls, *_, **__):
+    for inst in cls._instances:
+      inst.close()
+
+  def __init__(self, path, preserve=False, **sqlite3_connect_kwargs):
+    self._path = path
     self._isclosed = False
-    self._conn = sqlite3.connect(path)
-    self._conn.execute("""
-    create table if not exists Queue (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      data BLOB
-    )
-    """)
+    self._preserve = preserve
+    self._conn = sqlite3.connect(path, **sqlite3_connect_kwargs)
     self._cur0 = self._conn.cursor()
+    self._instances.append(self)
+
+  def __repr__(self):
+    return f'<sqlite3:{hex(id(self))}[{self._path}]>'
 
   def close(self):
+    self._thread_lock.acquire()
     if self._isclosed:
+      if self in self._instances:
+        self._instances.remove(self)
+      self._thread_lock.release()
       return
     print(f'closing conn: {self}')
     self._conn.commit()
     self._conn.close()
     self._isclosed = True
+    if not self._preserve and os.path.exists(self._path):
+      # !!!
+      os.unlink(self._path)
+    if self in self._instances:
+      self._instances.remove(self)
+    self._thread_lock.release()
+
+
+class SqlLite3Deque(_SqlLite3TempConn):
+
+  def __init__(self, path, preserve=False, **sqlite3_connect_kwargs):
+    super().__init__(path, preserve=preserve, **sqlite3_connect_kwargs)
+    self._conn.execute("""
+    create table if not exists Deque (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      data BLOB,
+      _ones INTEGER DEFAULT 1
+    )
+    """)
 
   @staticmethod
   def serializer(item):
@@ -32,32 +76,76 @@ class SqlLite3Queue:
   def deserializer(blob):
     return blob
 
-  def _get_last_id(self):
-    query = self._cur0.execute('select max(id) from Queue')
+  @sqlite3w_classmethod_closedconn_fallback_value(None)
+  def _get_first_id(self):
+    query = self._cur0.execute('select min(id) from Deque')
     return query.fetchone()[0]
 
+  @sqlite3w_classmethod_closedconn_fallback_value(None)
+  def _get_last_id(self):
+    query = self._cur0.execute('select max(id) from Deque')
+    return query.fetchone()[0]
+
+  @sqlite3w_classmethod_closedconn_fallback_value(None)
   def put(self, item):
-    self._cur0.execute('insert into Queue (data) values (?)', (self.serializer(item),))
+    self._cur0.execute('insert into Deque (data) values (?)', (self.serializer(item),))
     self._conn.commit()
 
+  @sqlite3w_classmethod_closedconn_fallback_value(None)
+  def append(self, item):
+    self.put(item)
+
+  @sqlite3w_classmethod_closedconn_fallback_value(False)
+  def has(self, item):
+    query = self._cur0.execute('select id from Deque where data=(?)', (self.serializer(item),))
+    return not query.fetchone() is None
+
+  @sqlite3w_classmethod_closedconn_fallback_value(None)
+  def _fetch_from_id(self, _id):
+    query = self._cur0.execute(f'select data from Deque where id={_id}')
+    blob, = query.fetchone()
+    self._cur0.execute(f'delete from Deque where id={_id}')
+    self._conn.commit()
+    return blob
+
+  @sqlite3w_classmethod_closedconn_fallback_value(None)
   def get(self):
     last_id = self._get_last_id()
     if last_id is None:
       return None
-    query = self._cur0.execute(f'select data from Queue where id={last_id}')
-    blob, = query.fetchone()
-    self._cur0.execute(f'delete from Queue where id={last_id}')
-    self._conn.commit()
+    blob = self._fetch_from_id(last_id)
     return self.deserializer(blob)
+
+  @sqlite3w_classmethod_closedconn_fallback_value(None)
+  def get_left(self):
+    last_id = self._get_first_id()
+    if last_id is None:
+      return None
+    blob = self._fetch_from_id(last_id)
+    return self.deserializer(blob)
+
+  def pop(self):
+    return self.get()
+
+  def popleft(self):
+    return self.get_left()
 
   def empty(self):
     return self._get_last_id() is None
 
+  def count(self):
+    if self.empty():
+      return 0
+    query = self._cur0.execute('select sum(_ones) from Deque')
+    return query.fetchone()[0]
+
+  def __len__(self):
+    return self.count()
+
+# backward compat
+SqlLite3Queue = SqlLite3Deque
 
 import signal, atexit
-def _sqlite3w_SqlLite3Queue_handle_closeall(*args, **kwargs):
-  for instance in SqlLite3Queue._instances:
-    instance.close()
-signal.signal(signal.SIGTERM, _sqlite3w_SqlLite3Queue_handle_closeall)
-signal.signal(signal.SIGINT, _sqlite3w_SqlLite3Queue_handle_closeall)
-atexit.register(_sqlite3w_SqlLite3Queue_handle_closeall)
+signal.signal(signal.SIGTERM, _SqlLite3TempConn._handle_closeAllOpenConnInstance)
+signal.signal(signal.SIGINT, _SqlLite3TempConn._handle_closeAllOpenConnInstance)
+atexit.register(_SqlLite3TempConn._handle_closeAllOpenConnInstance)
